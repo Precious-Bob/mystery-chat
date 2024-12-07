1d51a8c055a984c958c3523400f6f5d78554ef93
Precious <preciousbobmanuel501@gmail.com>
2024-12-07 14:18:28 +0100

    create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         logging: ['query', 'error', 'schema'],
       }),
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
