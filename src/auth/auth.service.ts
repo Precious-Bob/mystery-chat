81e9757ec10f036cffa0888719af85df5b39f613
Precious <preciousbobmanuel501@gmail.com>
2024-12-08 10:12:24 +0100

    signup and signin service fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { SigninDto, SignupDto } from 'src/dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+    @InjectRepository(UserEntity)
+    private readonly userRepo: Repository<UserEntity>,
+  ) {}
+
+  async signup(dto: SignupDto) {
+    try {
+      if (dto.password !== dto.confirmPassword) {
+        throw new BadRequestException('Passwords do not match!');
+      }
+      dto.password = await argon.hash(dto.password);
+      const user = this.userRepo.create(dto);
+      await user.save();
+      return this.signToken(user.id, user.email);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('Credentials alredy exists!');
+      }
+      throw e;
+    }
+  }
+
+  async signin({ email, password }: SigninDto) {
+    try {
+      const user = await this.userRepo.findOne({ where: { email } });
+      if (!user) throw new NotFoundException('Credentials incorrect');
+
+      const validPassword = await argon.verify(user.password, password);
+      if (!validPassword) throw new NotFoundException('Credentials incorrect');
+      return this.signToken(user.id, user.email);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async signToken(
+    userId: string,
+    email: string,
+  ): Promise<{ access_token: string; refresh_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const accessToken = await this.jwt.signAsync(payload, {
+      expiresIn: '10m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    const refreshToken = await this.jwt.signAsync(payload, {
+      expiresIn: '1d',
+      secret: this.config.get('JWT_REFRESH_SECRET'),
+    });
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
+  }
+}
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,37 +1,19 @@
 import { Column, Entity } from 'typeorm';
 import { AbstractEntity } from './abstract.entity';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsOptional,
-  IsStrongPassword,
-  MinLength,
-} from 'class-validator';
 import { Exclude } from 'class-transformer';
 
 @Entity()
 export class UserEntity extends AbstractEntity {
   @Column({ unique: true })
-  @IsNotEmpty()
-  @MinLength(3)
   username: string;
 
   @Column({ unique: true })
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
 
-  @Column({ nullable: true })
-  @IsOptional()
-  image: string | null;
-
   @Column()
-  @IsNotEmpty()
   @Exclude() // Add { toPlainOnly: true } if you want to work with the password
-  @IsStrongPassword()
   password: string;
 
   @Column({ nullable: true })
-  @IsOptional()
   profileLink: string | null;
 }
